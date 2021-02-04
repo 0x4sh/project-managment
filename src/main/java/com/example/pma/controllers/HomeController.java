b9dcc427b782ec3803eeb42fa62847148a9956e0
ashish <hiremath.ashish@tcs.com>
2021-02-04 11:02:05 +0530

    Updated Javascript code

--- src/main/java/com/example/pma/controllers/HomeController.java
+++ src/main/java/com/example/pma/controllers/HomeController.java
@@ -2,14 +2,20 @@ package com.example.pma.controllers;
 
 import com.example.pma.dao.EmployeeRepository;
 import com.example.pma.dao.ProjectRepository;
+import com.example.pma.dto.ChartData;
+import com.example.pma.dto.EmployeeProject;
 import com.example.pma.entities.Employee;
 import com.example.pma.entities.Project;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -21,15 +27,26 @@ public class HomeController {
     EmployeeRepository empRepo;
 
     @GetMapping("/")
-    public String displayHome(Model model) {
+    public String displayHome(Model model) throws JsonProcessingException {
+
+        Map<String,Object> map = new HashMap<>();
 
         // database query for projects
         List<Project> projects = proRepo.findAll();
         model.addAttribute("projectsList", projects);
 
+        List<ChartData> projectData = proRepo.getProjectStatus();
+
+        // Convert projectData object into a json structure for use in javascript
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(projectData);
+
+        // sending this jsonString to view via model
+        model.addAttribute("projectStatusCount",jsonString);
+
         // database query for employees
-        List<Employee> employees = empRepo.findAll();
-        model.addAttribute("employeesList", employees);
+        List<EmployeeProject> employeesProjectCount = empRepo.employeeProjects();
+        model.addAttribute("employeesProjectCount", employeesProjectCount);
 
         return "main/home";
     }
